0b36382f08f518a07be09b4957266da7135bdee0
Adam Kowalczyk <adam-kowalczyk@o2.pl>
2007-03-14 23:52:04 +0000

    Drop update.performAtStartup. Add update.lastUpdateTime for the new update timer system.

--- defaults/preferences/brief.js
+++ defaults/preferences/brief.js
@@ -3,8 +3,7 @@ pref("extensions.brief.showStatusbarIcon", true);
 pref("extensions.brief.openInNewTab", false);
 pref("extensions.brief.showFavicons", true);
 pref("extensions.brief.firstRun", true);
-pref("extensions.brief@mozdev.org.description",
-     "chrome://brief/locale/brief.properties");
+pref("extensions.brief@mozdev.org.description", "chrome://brief/locale/brief.properties");
 
 pref("extensions.brief.feedview.shownEntries", 'all');
 pref("extensions.brief.feedview.entriesPerPage", 10);
@@ -15,8 +14,8 @@ pref("extensions.brief.feedview.doubleClickMarks", true);
 pref("extensions.brief.feedview.linkMarksRead", true);
 
 pref("extensions.brief.update.interval", 60);
+pref("extensions.brief.update.lastUpdateTime", 0);
 pref("extensions.brief.update.enableAutoUpdate", true);
-pref("extensions.brief.update.performAtStartup", false);
 pref("extensions.brief.update.showNotification", true);
 pref("extensions.brief.update.defaultFetchDelay", 250);
 pref("extensions.brief.update.backgroundFetchDelay", 2000);
@@ -24,6 +23,6 @@ pref("extensions.brief.update.backgroundFetchDelay", 2000);
 pref("extensions.brief.database.expireEntries", false);
 pref("extensions.brief.database.entryExpirationAge", 60);
 pref("extensions.brief.database.limitStoredEntries", false);
-pref("extensions.brief.database.maxStoredEntries", 200);
+pref("extensions.brief.database.maxStoredEntries", 100);
 pref("extensions.brief.database.lastDeletedRedundantTime", 0);
 pref("extensions.brief.database.lastPurgeTime", 0);
