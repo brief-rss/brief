dcc8953444a83719cce175a6af84fb538a9f7687
Adam Kowalczyk <adam-kowalczyk@o2.pl>
2010-10-30 19:52:08 +0000

    Make Brief compatible with Mozilla 2.0 XPCOM component registration.

--- components/Service.js
+++ components/Service.js
@@ -3,12 +3,17 @@ const Ci = Components.interfaces;
 
 Components.utils.import('resource://gre/modules/XPCOMUtils.jsm');
 
-var Service = null;
-
 function BriefService() {
-    var observerService = Cc['@mozilla.org/observer-service;1']
-                          .getService(Ci.nsIObserverService);
-    observerService.addObserver(this, 'profile-after-change', false);
+    // Firefox 4 component registration.
+    if (XPCOMUtils.generateNSGetFactory) {
+        Components.utils.import('resource://brief/Storage.jsm');
+    }
+    // Old component registration.
+    else {
+        let observerService = Cc['@mozilla.org/observer-service;1']
+                              .getService(Ci.nsIObserverService);
+        observerService.addObserver(this, 'profile-after-change', false);
+    }
 }
 
 BriefService.prototype = {
@@ -16,11 +21,10 @@ BriefService.prototype = {
     // nsIObserver
     observe: function BriefService_observe(aSubject, aTopic, aData) {
         if (aData == 'startup') {
-            var observerService = Cc['@mozilla.org/observer-service;1']
+            let observerService = Cc['@mozilla.org/observer-service;1']
                                   .getService(Ci.nsIObserverService);
             observerService.removeObserver(this, 'profile-after-change');
 
-            // Initialize modules.
             Components.utils.import('resource://brief/Storage.jsm');
         }
     },
@@ -29,18 +33,15 @@ BriefService.prototype = {
     classID: Components.ID('{943b2280-6457-11df-a08a-0800200c9a66}'),
     contractID: '@brief.mozdev.org/briefservice;1',
     _xpcom_categories: [ { category: 'app-startup', service: true } ],
-    _xpcom_factory: {
-        createInstance: function(aOuter, aIID) {
-            if (aOuter != null)
-                throw Components.results.NS_ERROR_NO_AGGREGATION;
-
-            if (!Service)
-                Service = new BriefService();
-
-            return Service.QueryInterface(aIID);
-        }
-    },
     QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver])
 }
 
-function NSGetModule(compMgr, fileSpec) XPCOMUtils.generateModule([BriefService])
+
+/**
+ * XPCOMUtils.generateNSGetFactory was introduced in Mozilla 2 (Firefox 4).
+ * XPCOMUtils.generateNSGetModule is for Mozilla 1.9.2 (Firefox 3.6).
+ */
+if (XPCOMUtils.generateNSGetFactory)
+    var NSGetFactory = XPCOMUtils.generateNSGetFactory([BriefService]);
+else
+    var NSGetModule = XPCOMUtils.generateNSGetModule([BriefService]);
