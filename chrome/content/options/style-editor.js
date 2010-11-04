eae2f1e3fe482bd5089a95da78181c976d449bdc
Adam Kowalczyk <ancestor@o2.pl>
2010-11-04 21:07:26 +0000

    Fix: style editor broken in Firefox 4 due to new extension manager API.

--- chrome/content/options/style-editor.js
+++ chrome/content/options/style-editor.js
@@ -1,11 +1,15 @@
 const Cc = Components.classes;
 const Ci = Components.interfaces;
 
+Components.utils.import('resource://gre/modules/AddonManager.jsm');
+
 var gCustomStyleFile = null;
 var gTextbox = null;
 
 function init() {
     sizeToContent();
+
+    gTextbox = document.getElementById('custom-style-textbox');
 
     var chromeDir = Cc['@mozilla.org/file/directory_service;1'].
                     getService(Ci.nsIProperties).
@@ -15,38 +19,68 @@ function init() {
     gCustomStyleFile = chromeDir.clone();
     gCustomStyleFile.append('brief-custom-style.css');
 
-    // If the custom CSS file doesn't exist, create it by copying the example file.
-    if (!gCustomStyleFile.exists()) {
-        var exampleCustomStyle = Cc['@mozilla.org/extensions/manager;1'].
-                                 getService(Ci.nsIExtensionManager).
-                                 getInstallLocation('brief@mozdev.org').
-                                 getItemLocation('brief@mozdev.org');
-        exampleCustomStyle.append('defaults');
-        exampleCustomStyle.append('data');
-        exampleCustomStyle.append('example-custom-style.css');
-        exampleCustomStyle.copyTo(chromeDir, 'brief-custom-style.css');
+    if ('@mozilla.org/extensions/manager;1' in Cc) {
+        // If the custom CSS file doesn't exist, create it by copying the example file.
+        if (!gCustomStyleFile.exists()) {
+            let exampleCustomStyle = Cc['@mozilla.org/extensions/manager;1']
+                                     .getService(Ci.nsIExtensionManager)
+                                     .getInstallLocation('brief@mozdev.org')
+                                     .getItemLocation('brief@mozdev.org');
+            exampleCustomStyle.append('defaults');
+            exampleCustomStyle.append('data');
+            exampleCustomStyle.append('example-custom-style.css');
+            exampleCustomStyle.copyTo(chromeDir, 'brief-custom-style.css');
+        }
+
+        populateTextbox();
     }
+    else {
+        if (!gCustomStyleFile.exists()) {
+            gCustomStyleFile.create(Ci.nsIFile.NORMAL_FILE_TYPE, 777);
 
-    var uri = Cc['@mozilla.org/network/protocol;1?name=file'].
-              getService(Ci.nsIFileProtocolHandler).
-              newFileURI(gCustomStyleFile);
+            AddonManager.getAddonByID('brief@mozdev.org', function(addon) {
+                let uri = addon.getResourceURI('/defaults/data/example-custom-style.css');
+                let cssText = fetchCSSText(uri);
+                writeCustomCSSFile(cssText);
+                gTextbox.value = cssText;
+            })
+        }
+        else {
+            populateTextbox();
+        }
+    }
+}
 
+
+function populateTextbox() {
+    let uri = Cc['@mozilla.org/network/protocol;1?name=file']
+              .getService(Ci.nsIFileProtocolHandler)
+              .newFileURI(gCustomStyleFile);
+    gTextbox.value = fetchCSSText(uri);
+}
+
+
+function fetchCSSText(aURI) {
     var request = new XMLHttpRequest();
-    request.open('GET', uri.spec, false);
+    request.open('GET', aURI.spec, false);
     request.overrideMimeType('text/css');
     request.send(null);
 
-    gTextbox = document.getElementById('custom-style-textbox');
-    gTextbox.value = request.responseText;
+    return request.responseText;
+}
+
+
+function writeCustomCSSFile(aData) {
+    var stream = Cc['@mozilla.org/network/file-output-stream;1']
+                 .createInstance(Ci.nsIFileOutputStream);
+    stream.init(gCustomStyleFile, 0x02 | 0x08 | 0x20, -1, 0); // write, create, truncate
+    stream.write(aData, aData.length);
+    stream.close();
 }
 
 
 function onAccept() {
-    var stream = Cc['@mozilla.org/network/file-output-stream;1'].
-                 createInstance(Ci.nsIFileOutputStream);
-    stream.init(gCustomStyleFile, 0x02 | 0x08 | 0x20, -1, 0); // write, create, truncate
-    stream.write(gTextbox.value, gTextbox.value.length);
-    stream.close();
+    writeCustomCSSFile(gTextbox.value, gTextbox.value.length);
 
     var observerService = Cc['@mozilla.org/observer-service;1'].
                           getService(Ci.nsIObserverService);
