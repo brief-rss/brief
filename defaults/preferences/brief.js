bcf844fcb7dbcf77963632e06494c57aaffd9aba
Adam Kowalczyk <adam-kowalczyk@o2.pl>
2010-12-20 22:30:22 +0000

    Enable entry selection by default.

--- defaults/preferences/brief.js
+++ defaults/preferences/brief.js
@@ -14,7 +14,7 @@ pref("extensions.brief.feedview.filterStarred", false);
 pref("extensions.brief.feedview.openEntriesInTabs", true);
 pref("extensions.brief.feedview.doubleClickMarks", true);
 pref("extensions.brief.feedview.autoMarkRead", false);
-pref("extensions.brief.feedview.entrySelectionEnabled", false);
+pref("extensions.brief.feedview.entrySelectionEnabled", true);
 pref("extensions.brief.feedview.sortUnreadViewOldestFirst", false);
 
 pref("extensions.brief.update.interval", 3600);
