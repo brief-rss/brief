a8366ebb702f9bade8e3876a8ec4a20410d4c847
Denis Lisov <dennis.lissov@gmail.com>
2017-11-12 04:08:37 +0300

    i18n: fallback to message name

--- scripts/i18n.js
+++ scripts/i18n.js
@@ -1,13 +1,13 @@
 // Perform substitutions for i18n in text and attributes
 function apply_i18n(doc) {
     for(let node of document.querySelectorAll('[data-i18n]')) {
-        text = browser.i18n.getMessage(node.dataset.i18n) || node.dataset.i18n;
+        let text = browser.i18n.getMessage(node.dataset.i18n) || node.dataset.i18n;
         node.appendChild(document.createTextNode(text));
     }
     for(let node of document.querySelectorAll('[data-i18n-attrs]')) {
         for(let substitution of node.dataset.i18nAttrs.split(/\s+/g)) {
             let [attr, text] = substitution.split(':');
-            text = browser.i18n.getMessage(text);
+            text = browser.i18n.getMessage(text) || text;
             node[attr] = text;
         }
     }
