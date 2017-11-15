5d6cca87ab069b08dfc42d41c1f8221fac146c7a
Denis Lisov <dennis.lissov@gmail.com>
2017-11-15 11:03:18 +0300

    move old tooltip code to background.js

--- background.js
+++ background.js
@@ -40,7 +40,10 @@ const Brief = {
         await Prefs.init({master: true});
 
         Prefs.addObserver('showUnreadCounter', () => this._updateUI());
-        //FIXME: update UI on db changes
+        Comm.registerObservers({
+            'feedlist-updated': () => this._updateUI(),
+            'entries-updated': () => this._updateUI(), //TODO: there was a debounce here...
+        });
 
         await Database.init();
 
@@ -85,6 +88,98 @@ const Brief = {
             browser.browserAction.setBadgeText({text: ""});
         }
         //TODO: return tooltip
+        /*
+            _updateStatus: async function Brief__updateStatus() {
+                let updated = "";
+                let bundle = Services.strings.createBundle('chrome://brief/locale/brief.properties');
+
+                let lastUpdateTime = this.prefs.getIntPref('update.lastUpdateTime') * 1000;
+                let date = new Date(lastUpdateTime);
+                let relativeDate = new this.common.RelativeDate(lastUpdateTime);
+
+                let time, pluralForms, form;
+                let lang = Brief.window.navigator.language;
+
+                switch (true) {
+                    case relativeDate.deltaMinutes === 0:
+                        updated = bundle.GetStringFromName('lastUpdated.rightNow');
+                        break;
+
+                    case relativeDate.deltaHours === 0:
+                        pluralForms = bundle.GetStringFromName('minute.pluralForms');
+                        form = this.common.getPluralForm(relativeDate.deltaMinutes, pluralForms);
+                        updated = bundle.formatStringFromName('lastUpdated.ago', [form], 1)
+                                            .replace('#number', relativeDate.deltaMinutes);
+                        break;
+
+                    case relativeDate.deltaHours <= 12:
+                        pluralForms = bundle.GetStringFromName('hour.pluralForms');
+                        form = this.common.getPluralForm(relativeDate.deltaHours, pluralForms);
+                        updated = bundle.formatStringFromName('lastUpdated.ago', [form], 1)
+                                            .replace('#number', relativeDate.deltaHours);
+                        break;
+
+                    case relativeDate.deltaDaySteps === 0:
+                        time = date.toLocaleTimeString(lang, {hour: 'numeric', minute: 'numeric'});
+                        updated = bundle.formatStringFromName('lastUpdated.today', [time], 1);
+                        break;
+
+                    case relativeDate.deltaDaySteps === 1:
+                        time = date.toLocaleTimeString(lang, {hour: 'numeric', minute: 'numeric'});
+                        updated = bundle.formatStringFromName('lastUpdated.yesterday', [time], 1);
+                        break;
+
+                    case relativeDate.deltaDaySteps < 7:
+                        pluralForms = bundle.GetStringFromName('day.pluralForms');
+                        form = this.common.getPluralForm(relativeDate.deltaDays, pluralForms);
+                        updated = bundle.formatStringFromName('lastUpdated.ago', [form], 1)
+                                            .replace('#number', relativeDate.deltaDays);
+                        break;
+
+                    case relativeDate.deltaYearSteps === 0:
+                        date = date.toLocaleDateString(lang, {month: 'long', day: 'numeric'});
+                        updated = bundle.formatStringFromName('lastUpdated.fullDate', [date], 1);
+                        break;
+
+                    default:
+                        date = date.toLocaleDateString(lang, {
+                            year: 'numeric', month: 'long', day: 'numeric'});
+                        updated = bundle.formatStringFromName('lastUpdated.fullDate', [date], 1);
+                        break;
+                }
+
+                let rows = [];
+
+                let feeds_query = new Query({
+                    deleted: false,
+                    read: false,
+                    sortOrder: 'library',
+                    sortDirection: 'asc'
+                })
+
+                let unreadFeeds = await feeds_query.getProperty('feedID', true);
+
+                let noUnreadText = "";
+                if(unreadFeeds.length == 0)
+                    noUnreadText = bundle.GetStringFromName('noUnreadFeedsTooltip');
+
+                for (let feed of unreadFeeds) {
+                    let feedName = Storage.getFeed(feed).title;
+                    if(feedName.length > 24)
+                        feedName = feedName.substring(0, 24) + "...";
+
+                    let query = new Query({
+                        deleted: false,
+                        feeds: [feed],
+                        read: false
+                    })
+
+                    rows.push(query.getEntryCount().then(count => `${count}\t\t${feedName}`));
+                }
+                rows = await Promise.all(rows);
+                let tooltip = `${updated}\n\n${noUnreadText}${rows.join('\n')}`;
+            },
+         */
         //browser.browserAction.setTitle({title: tooltip});
     },
 };
