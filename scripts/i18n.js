87067970dc50a5b783f1d1be582bf5c53c628732
Denis Lisov <dennis.lissov@gmail.com>
2017-11-17 02:13:23 +0300

    limited entites support for i18n

--- scripts/i18n.js
+++ scripts/i18n.js
@@ -4,14 +4,26 @@
 function apply_i18n(doc) {
     for(let node of document.querySelectorAll('[data-i18n]')) {
         let text = browser.i18n.getMessage(node.dataset.i18n) || node.dataset.i18n;
-        node.appendChild(document.createTextNode(text));
+        node.appendChild(document.createTextNode(replaceEntities(text)));
     }
     for(let node of document.querySelectorAll('[data-i18n-attrs]')) {
         for(let substitution of node.dataset.i18nAttrs.split(/\s+/g)) {
             let [attr, text] = substitution.split(':');
             text = browser.i18n.getMessage(text) || text;
-            node.setAttribute(attr, text);
+            node.setAttribute(attr, replaceEntities(text));
         }
+    }
+
+    function replaceEntities(str) {
+        let entities = {
+            '&apos;': "'",
+            '&lt;': "<",
+            '&gt;': ">",
+        };
+        for(let [s, d] of Object.entries(entities)) {
+            str = str.replace(s, d);
+        }
+        return str;
     }
 }
 
