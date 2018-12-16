444544166d03211855e4544b756efcd68cfc1e4d
Denis Lisov <dennis.lissov@gmail.com>
2018-12-17 01:23:19 +0300

    scan-for-feeds: detect feed roots on a page
    Since Firefox 64 there's no feed preview and the content script is being
    loaded into the feed XMLs. Detect that situation and strongly suggest
    subscribing to them.

--- content_scripts/scan-for-feeds.js
+++ content_scripts/scan-for-feeds.js
@@ -1,6 +1,40 @@
 'use strict';
 
 (function() {
+    // Are we actually seeing a feed page?
+    function isFeedPage() {
+        // In a frame or view-source: this script is not loaded
+        // Cannot check for GET/POST without intercepting the request
+        const FEED_TYPES = [
+            "application/atom+xml",
+            "application/rss+xml",
+        ];
+        if(FEED_TYPES.includes(document.contentType)) {
+            return true;
+        }
+        // contentType - Firefox was checking only
+        // TEXT_HTML, APPLICATION_OCTET_STREAM and XML-mentioning ones
+        const ROOTS = "rss:root, feed:root, RDF:root"; // Also see ROOTS in scripts/updater.js
+        let root = document.querySelector(ROOTS);
+        if(root !== null) {
+            if(root.localName == "RDF") {
+                return Array.from(root.attributes).some(
+                    attr => attr.value == "http://purl.org/rss/1.0/"
+                    // Firefox used to grep also for
+                    // http://www.w3.org/1999/02/22-rdf-syntax-ns#
+                );
+            }
+            return true;
+        }
+        return false;
+    }
+    if(isFeedPage()) {
+        let TITLE = "RDF > *|title, channel > *|title, *|feed > *|title";
+        let linkTitle = document.querySelector(TITLE).textContent || "";
+        return [{linkTitle, url: document.location.href, kind: 'self'}];
+    }
+
+    // Ok, not a feed page itself, any links to feeds?
     // Inspired by Firefox's getFeedsInfo in `browser/base/content/content.js`
     let links = document.querySelectorAll(
         'link[rel~=feed], link[rel~=alternate]:not([rel~=stylesheet])');
