cad56620ad1d70b0dc258b34b3c679caa38c85d3
Denis Lisov <dennis.lissov@gmail.com>
2020-01-07 16:26:39 +0300

    request-monitor: add content sniffer for feed tags
    Since Firefox dropped its feed-related code in version 64 Brief has been
    using a content script to inject into every page and check whether the
    page is actually a feed. This kind of detection is basically required as
    a significant number of servers fail to serve RSS/ATOM with the correct
    MIME-type, and also pretty cheap as Brief has to inject its content
    script anyway to detect links to feeds in the page.
    
    This trick works for most pages. However, some websites use an
    XSLT-based rendering for their feeds by specifying an `xml-stylesheet`
    processing instruction that causes the browser to rewrite the XML. This
    means that by the time content sctipt is loaded the original markup is
    no longer there, so one cannot detect a feed at that stage.
    
    To deal with this (and also speed up feed detection for other pages by
    doing it earlier in the pipeline) this commit introduces a feed sniffer
    modeled after the legacy Firefox sniffer. It parses the first 512 bytes
    of the response and, if it looks like a valid XML and has a root tag
    specific to feeds (and contains the RSS namespace URI in the RSS 1.0
    case), triggers the redirect directly from the request monitor.

--- modules/request-monitor.js
+++ modules/request-monitor.js
@@ -1,3 +1,5 @@
+import {tryGetXmlRootName} from "./xml-sniffer.js";
+
 export function init() {
     console.debug("Initializing the feed request monitor");
     browser.webRequest.onHeadersReceived.addListener(
@@ -26,27 +28,82 @@ const KNOWN_FEED_TYPES = [
     'application/atom+xml',
     'application/rss+xml',
 ];
-/*
+
 // Let's not consider 'application/octet-stream' for now
-// and leave the XML-based MIME types for the content script
 const MAYBE_FEED_TYPES = [
-    'application/octet-stream',
+    //'application/octet-stream',
     'application/xml',
     'text/html',
     'text/xml',
     // and anything with `+xml` as a special case
 ];
-*/
 
-function headerCheck({tabId, url, responseHeaders}) {
+const FEED_ROOT_TAGS = ['rss', 'feed', 'rdf:RDF'];
+const SNIFF_WINDOW = 512; // Matches the legacy Firefox feed sniffer window
+
+
+function headerCheck({requestId, tabId, url, responseHeaders}) {
     let contentType = responseHeaders
         .filter(h => h.name.toLowerCase() == 'Content-Type'.toLowerCase())
         .filter(h => h.value !== undefined)
         .map(h => h.value)[0];
-    let {mime} = parseContentType(contentType);
+    let {mime, encoding} = parseContentType(contentType);
     if(KNOWN_FEED_TYPES.includes(mime)) {
         let previewUrl = "/ui/brief.xhtml?preview=" + encodeURIComponent(url);
         browser.tabs.update(tabId, {url: previewUrl}); // No loadReplace: still on old page
         return {cancel: true};
+    }
+    if(MAYBE_FEED_TYPES.includes(mime) || mime.includes('+xml')) {
+        let filter = browser.webRequest.filterResponseData(requestId);
+        let chunks = [];
+
+        filter.ondata = ({data}) => {
+            filter.write(data);
+            chunks.push(data);
+            let totalBytes = chunks.map(c => c.byteLength).reduce((a, b) => a + b, 0);
+            if(totalBytes >= SNIFF_WINDOW) {
+                checkContent(chunks, {encoding, url, tabId});
+                filter.disconnect();
+            }
+        };
+        filter.onstop = () => {
+            checkContent(chunks, {encoding, url, tabId});
+            filter.close();
+        };
+    }
+}
+
+function checkContent(buffers, {encoding, url, tabId}) {
+    let decoder;
+    try {
+        decoder = new TextDecoder(encoding, {fatal: true});
+    } catch(e) {
+        return; // Invalid encoding, cannot decode the feed for checking
+    }
+    let text = "";
+    let bytesRemaining = SNIFF_WINDOW;
+    for(let buffer of buffers.slice(0, -1)) {
+        try {
+            text += decoder.decode(buffer, {stream: true});
+        } catch(e) {
+            return; // Byte stream invalid for the encoding specified
+        }
+        bytesRemaining -= buffer.byteLength;
+    }
+    text += decoder.decode(
+        buffers[buffers.length - 1].slice(0, bytesRemaining),
+        {stream: true},
+    );
+
+    let rootTag = tryGetXmlRootName(text);
+
+    if(FEED_ROOT_TAGS.includes(rootTag)) {
+        if(rootTag === 'rdf:RDF' && !text.includes("http://purl.org/rss/1.0/")) {
+            // This RDF is probably not a feed, skipping it
+            return;
+        }
+        console.log('feed detected, redirecting to preview page for', url);
+        let previewUrl = "/ui/brief.xhtml?preview=" + encodeURIComponent(url);
+        browser.tabs.update(tabId, {url: previewUrl, loadReplace: true});
     }
 }
