343350885e84c9565bd6afcfa51bf6bda5da58fe
Denis Lisov <dennis.lissov@gmail.com>
2024-05-13 01:19:03 +0300

    modules/i18n.js: add minimal types

--- modules/i18n.js
+++ modules/i18n.js
@@ -1,6 +1,10 @@
-// Perform substitutions for i18n in text and attributes
+/**
+ * Perform substitutions for i18n in text and attributes
+ *
+ * @param {Document} doc
+ */
 export function apply_i18n(doc) {
-    for(let node of doc.querySelectorAll('[data-i18n]')) {
+    for(let node of /** @type NodeListOf<HTMLElement> */(doc.querySelectorAll('[data-i18n]'))) {
         let text = browser.i18n.getMessage(node.dataset.i18n) || node.dataset.i18n;
         if(node.dataset.i18nAllowMarkup !== undefined) {
             node.insertAdjacentHTML('beforeend', text);
@@ -8,7 +12,7 @@ export function apply_i18n(doc) {
             node.insertAdjacentText('beforeend', text);
         }
     }
-    for(let node of doc.querySelectorAll('[data-i18n-attrs]')) {
+    for(let node of /** @type NodeListOf<HTMLElement> */(doc.querySelectorAll('[data-i18n-attrs]'))) {
         for(let substitution of node.dataset.i18nAttrs.trim().split(/\s+/g)) {
             let [attr, text] = substitution.split(':');
             text = browser.i18n.getMessage(text) || text;
