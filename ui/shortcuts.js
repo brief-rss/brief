52778890bfb8507c8f1dc52a05b883238509aeac
Denis Lisov <dennis.lissov@gmail.com>
2024-05-18 02:34:05 +0300

    shortcuts: simplify js

--- ui/shortcuts.js
+++ ui/shortcuts.js
@@ -3,13 +3,7 @@ import {apply_i18n} from "/modules/i18n.js";
 
 async function onload() {
     apply_i18n(document);
-    let elems = window.navigator.platform.match('Mac')
-        ? document.getElementsByClassName('noMac')
-        : document.getElementsByClassName('onlyMac');
-
-    for (let i = 0; i < elems.length; i++) {
-        (/** @type HTMLElement */ (elems[i])).style.display = 'none';
-    }
+    document.body.classList.toggle('platform-mac', window.navigator.platform.match('Mac') !== null);
 
     // Workaround for mozilla bug 1408446
     let {id, height} = await browser.windows.getCurrent();
