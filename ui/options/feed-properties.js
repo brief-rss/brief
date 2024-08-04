f63bd3661d32018bb0081bf184c94877022210c6
Denis Lisov <dennis.lissov@gmail.com>
2024-08-05 01:32:56 +0300

    drop really outdated Firefox bug workarounds

--- ui/options/feed-properties.js
+++ ui/options/feed-properties.js
@@ -101,15 +101,6 @@ async function init() {
     );
 
     Enabler.init();
-
-    // Workaround for mozilla bug 1408446 (Firefox before 62 / non-OOP) and size correctly
-    let {id, height, width} = await browser.windows.getCurrent();
-    let html = document.documentElement;
-    console.log(html.scrollHeight, html.offsetHeight);
-    await browser.windows.update(id, {
-        height: height + html.scrollHeight - window.innerHeight,
-        width: width + html.scrollWidth - window.innerWidth,
-    });
 }
 
 function updateScale() {
--- ui/shortcuts.js
+++ ui/shortcuts.js
@@ -4,10 +4,6 @@ import {apply_i18n} from "/modules/i18n.js";
 async function onload() {
     apply_i18n(document);
     document.body.classList.toggle('platform-mac', window.navigator.platform.match('Mac') !== null);
-
-    // Workaround for mozilla bug 1408446
-    let {id, height} = await browser.windows.getCurrent();
-    await browser.windows.update(id, {height: height + 1});
 }
 
 function onKeyup({key}) {
