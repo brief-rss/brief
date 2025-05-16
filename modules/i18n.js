f60895242041d5a3eb3c2e980cd41e7831026971
Denis Lisov <dennis.lissov@gmail.com>
2025-05-17 01:38:36 +0300

    modules/i18n.js: @ts-strict

--- modules/i18n.js
+++ modules/i18n.js
@@ -1,3 +1,4 @@
+//@ts-strict
 /**
  * Perform substitutions for i18n in text and attributes
  *
@@ -5,7 +6,8 @@
  */
 export function apply_i18n(doc) {
     for(let node of /** @type NodeListOf<HTMLElement> */(doc.querySelectorAll('[data-i18n]'))) {
-        let text = browser.i18n.getMessage(node.dataset.i18n) || node.dataset.i18n;
+        let id = /** @type string */(node.dataset.i18n);
+        let text = browser.i18n.getMessage(id) || id;
         if(node.dataset.i18nAllowMarkup !== undefined) {
             node.insertAdjacentHTML('beforeend', text);
         } else {
@@ -13,7 +15,8 @@ export function apply_i18n(doc) {
         }
     }
     for(let node of /** @type NodeListOf<HTMLElement> */(doc.querySelectorAll('[data-i18n-attrs]'))) {
-        for(let substitution of node.dataset.i18nAttrs.trim().split(/\s+/g)) {
+        let spec = /** @type string */(node.dataset.i18nAttrs);
+        for(let substitution of spec.trim().split(/\s+/g)) {
             let [attr, text] = substitution.split(':');
             text = browser.i18n.getMessage(text) || text;
             node.setAttribute(attr, text);
