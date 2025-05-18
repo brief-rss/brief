78ab91e1938fda5bfb3e88789e254c7a087d96ce
valverde <manolo.valverde@gmail.com>
2025-05-18 18:46:52 +0900

    Make the app robust against rss files with wrong entity encodings.

--- modules/feed-fetcher.js
+++ modules/feed-fetcher.js
@@ -1,6 +1,6 @@
 //@ts-strict
 import {parseFeed} from "./feed-parser.js";
-import {wait, xhrPromise} from "./utils.js";
+import {wait, xhrPromise, cleanEntities} from "./utils.js";
 
 const DEFAULT_TIMEOUT = 25000; // Default fetch timeout
 
@@ -21,9 +21,28 @@ export async function fetchFeed(feed, {allow_cached = false} = {}) {
         xhrPromise(request).catch(() => undefined),
         wait(DEFAULT_TIMEOUT),
     ]);
+
     if(!doc) {
-        console.error("failed to fetch", url);
-        return;
+        if (request.status == 200) {
+            // Retry the request but parsing the response by hand cleaning named entities.
+            let rqst = new XMLHttpRequest();
+            rqst.open('GET', url);
+            rqst.overrideMimeType('application/xml');
+            if(!allow_cached) {
+		rqst.setRequestHeader('Cache-control', 'no-cache');
+	    }
+	    rqst.responseType = 'text';
+	    let text = await Promise.race([
+		xhrPromise(rqst).catch(() => undefined),
+		wait(DEFAULT_TIMEOUT),
+	    ]);
+	    const parser = new DOMParser();
+	    doc = parser.parseFromString(cleanEntities(text), 'application/xml');
+	}
+	else {
+            console.error("failed to fetch", url);
+            return;
+	}
     }
 
     if(doc.documentElement.localName === 'parseerror') {
