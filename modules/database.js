brief-rss/brief

commit 6352b41b69a4fe64db0836e66069fe1eb0a46cd8

more minor typing improvements

modules/database.js
@@ -50,7 +50,7 @@ export class Database {
 
     /**
      * @param {string} feedID
-     * @returns {Feed} feed
+     * @returns {Feed | undefined} feed
      */
     getFeed(feedID) {
         let feed = this.feeds.filter(f => f.feedID === feedID)[0];
@@ -507,7 +507,10 @@ export class Database {
         await this.modifyFeed(updates);
     }
 
-    async expireEntries(feeds) {
+    /**
+     * @param {Feed | Feed[] | null} feeds
+     */
+    async expireEntries(feeds=null) {
         if(!Comm.master) {
             return Comm.callMaster('entries-expire', {feeds});
         }
@@ -600,7 +603,7 @@ export class Database {
         return newEntries;
     }
 
-    async _pushEntries({entries, tx=undefined}) {
+    async _pushEntries({entries, tx=null}) {
         if(entries.length === 0) {
             return;
         }

modules/prefs.js
@@ -52,7 +52,7 @@ export let Prefs = {
 
     /**
      * @param {string} name
-     * @param {string} actionName
+     * @param {'update' | 'reset'} actionName
      */
     set: async function(name, value, actionName='update') {
         if(!Comm.master) {

modules/utils.js
@@ -291,7 +291,7 @@ export let Comm = {
 /**
  * FIXME get rid of `any` here
  * @typedef {object} HandlerTypes
- * @property {(arg: {feeds: any}) => void} entries-expire
+ * @property {(arg: {feeds: Feed | Feed[] | null}) => void} entries-expire
  * @property {(arg: {feeds: any, entries: any, changes: any}) => void} entries-updated
  * @property {(arg: {feeds: any, options: any}) => void} feedlist-add
  * @property {(arg: {feeds: Feed | Feed[]}) => void} feedlist-delete
@@ -299,7 +299,7 @@ export let Comm = {
  * @property {(arg: {updates: (FeedUpdate | FeedUpdate[])}) => void} feedlist-modify
  * @property {(arg: {feeds: Feed[]}) => void} feedlist-updated
  * @property {() => Promise<boolean>} is-options-window-open
- * @property {(arg: {name: string, value: string | boolean | number, actionName: any}) => void} set-pref
+ * @property {(arg: {name: string, value: string | boolean | number, actionName: 'update' | 'reset'}) => void} set-pref
  * @property {() => void} style-updated
  * @property {(arg: {feed: {url: string}}) => (string[])} subscribe-add-feed
  * @property {(arg: {windowId: number}) => ({url: string?, linkTitle: string?}[])} subscribe-get-feeds

commit 16a7ef728f5a8cd70a9fbf648a85539d04f12792

fixup: this was broken in 6352b41b6

modules/database.js
@@ -661,7 +661,7 @@ export class Database {
         let allEntries = [];
         let newEntries = []; // For update notification
 
-        if(tx === undefined) {
+        if(tx == null) {
             tx = this._db.transaction(['entries', 'revisions'], 'readwrite');
         }
         // Scan 1: every entry with IDs provided