54b46783c27b9cfdc27cd263d6d9c5098b628216
Denis Lisov <dennis.lissov@gmail.com>
2025-06-09 00:42:02 +0300

    subscribe.js: don't try subscribing from incognito

--- ui/subscribe/subscribe.js
+++ ui/subscribe/subscribe.js
@@ -1,29 +1,34 @@
-import {Comm} from "/modules/utils.js";
-
+import {Comm, previewUrl} from "/modules/utils.js";
 
 async function onload() {
-    let {id: windowId} = await browser.windows.getCurrent();
+    let {id: windowId, incognito} = await browser.windows.getCurrent();
+    let [{id: tabId}] = await browser.tabs.query({active: true, windowId});
 
     let feeds = await Comm.callMaster('subscribe-get-feeds', {windowId});
     if(feeds.length === 0) {
         console.log('Nothing to subscribe to');
         return;
-    } else if(feeds.length === 1) {
+    }
+    if(feeds.length === 1 && !incognito) {
         Comm.callMaster('subscribe-add-feed', {feed: feeds[0]});
         window.close();
         return;
-    } else {
-        for(let feed of feeds) {
-            let node = document.createElement('span');
-            node.className ="feed";
-            node.textContent = feed.linkTitle;
-            node.title = feed.url;
-            node.addEventListener('click', () => {
+    }
+
+    for(let feed of feeds) {
+        let node = document.createElement('span');
+        node.className ="feed";
+        node.textContent = feed.linkTitle;
+        node.title = feed.url;
+        node.addEventListener('click', () => {
+            if(incognito) {
+                browser.tabs.update(tabId, {url: previewUrl(feed.url)});
+            } else {
                 Comm.callMaster('subscribe-add-feed', {feed});
-                window.close();
-            });
-            document.body.appendChild(node);
-        }
+            }
+            window.close();
+        });
+        document.body.appendChild(node);
     }
 }
 
